6440cfefa0628117cdc44447297bf5d76687cacb
Dmytro Bielousov <dmytro.bielousov@codeandcakes.com>
2025-01-16 11:53:27 +0200

    feat(search-suits): move to link

--- src/search-suits/search-suits.service.ts
+++ src/search-suits/search-suits.service.ts
@@ -1,56 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { SearchSuit } from '@prisma/client';
 
 import { PrismaService } from '../global';
 import { CreateSearchSuitDto } from './dto/create-search-suit.dto';
 import { UpdateSearchSuitDto } from './dto/update-search-suit.dto';
-import { SearchSuit } from './types/search-suit.types';
 
 @Injectable()
 export class SearchSuitsService {
   constructor(private readonly db: PrismaService) {}
 
   create(d: CreateSearchSuitDto): Promise<SearchSuit> {
-    const data: Dto<SearchSuit> = {
-      name: d.name,
-      active: d.active,
-      companyId: d.companyId,
-      value: {
-        budget: d.budget,
-        duration: d.duration,
-        skills: d.skills,
-        keywords: d.keywords,
-        paymentVerified: true,
-        locations: d.locations,
-        expert: d.expert,
-        excludeKeywords: d.excludeKeywords,
+    return this.db.searchSuit.create({
+      data: {
+        name: d.name,
+        active: d.active,
+        companyId: d.companyId,
+        link: d.link,
       },
-    };
-
-    return this.db.searchSuit.create({ data: data as any }) as any;
+    });
   }
 
   async update(id: Id, d: UpdateSearchSuitDto): Promise<SearchSuit> {
     const searchSuit = await this.findOne(id);
 
-    const data: UpdateDto<SearchSuit> = {
-      name: d.name ?? searchSuit.name,
-      active: d.active ?? searchSuit.active,
-      value: {
-        budget: d.budget ?? searchSuit.value.budget,
-        duration: d.duration,
-        skills: d.skills,
-        keywords: d.keywords,
-        paymentVerified: true,
-        locations: d.locations,
-        excludeKeywords: d.excludeKeywords,
-        expert: d.expert || false,
-      },
-    };
-
     return this.db.searchSuit.update({
       where: { id },
-      data: data as any,
-    }) as any;
+      data: {
+        name: d.name ?? searchSuit.name,
+        active: d.active ?? searchSuit.active,
+        link: d.link ?? searchSuit.link,
+      },
+    });
   }
 
   findManyByCompany(companyId: Id): Promise<SearchSuit[]> {
